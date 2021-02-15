913193ecab08141555a15bdc29dc06b858d7c238
Fastering <73803835+Fastering18@users.noreply.github.com>
2021-02-15 18:13:24 +0700

    Visual Studio 2017 Resource

---
+++ Projek/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Goblox
+{
+    public partial class Form1 : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GenerateBTN = new System.Windows.Forms.Button();
+            this.InputState = new System.Windows.Forms.TextBox();
+            this.InputDetail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.BantuanDetailInput = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Instruction1 = new System.Windows.Forms.Label();
+            this.akunPengguna = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GenerateBTN
+            // 
+            this.GenerateBTN.AutoSize = true;
+            this.GenerateBTN.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerateBTN.Location = new System.Drawing.Point(304, 351);
+            this.GenerateBTN.Name = "GenerateBTN";
+            this.GenerateBTN.Size = new System.Drawing.Size(146, 36);
+            this.GenerateBTN.TabIndex = 0;
+            this.GenerateBTN.Text = "Luncurkan";
+            this.GenerateBTN.UseVisualStyleBackColor = true;
+            this.GenerateBTN.Click += new System.EventHandler(this.GenerateBTN_Click);
+            // 
+            // InputState
+            // 
+            this.InputState.Location = new System.Drawing.Point(200, 285);
+            this.InputState.Multiline = true;
+            this.InputState.Name = "InputState";
+            this.InputState.Size = new System.Drawing.Size(360, 34);
+            this.InputState.TabIndex = 1;
+            // 
+            // InputDetail
+            // 
+            this.InputDetail.Location = new System.Drawing.Point(217, 149);
+            this.InputDetail.Multiline = true;
+            this.InputDetail.Name = "InputDetail";
+            this.InputDetail.Size = new System.Drawing.Size(328, 20);
+            this.InputDetail.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(300, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Details Presence";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(300, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "State Presence";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // BantuanDetailInput
+            // 
+            this.BantuanDetailInput.AutoSize = true;
+            this.BantuanDetailInput.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BantuanDetailInput.Location = new System.Drawing.Point(197, 114);
+            this.BantuanDetailInput.Name = "BantuanDetailInput";
+            this.BantuanDetailInput.Size = new System.Drawing.Size(363, 16);
+            this.BantuanDetailInput.TabIndex = 6;
+            this.BantuanDetailInput.Text = "Detail yang akan muncul sebagai subjudul dari rich presence.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(197, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(369, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "State yang akan muncul sebagai isi konten dari rich presence.";
+            // 
+            // Instruction1
+            // 
+            this.Instruction1.AutoSize = true;
+            this.Instruction1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instruction1.Location = new System.Drawing.Point(9, 425);
+            this.Instruction1.Name = "Instruction1";
+            this.Instruction1.Size = new System.Drawing.Size(401, 16);
+            this.Instruction1.TabIndex = 8;
+            this.Instruction1.Text = "*Aplikasi discord harus terinstall untuk menggunakan rich presence.";
+            // 
+            // akunPengguna
+            // 
+            this.akunPengguna.AutoSize = true;
+            this.akunPengguna.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.akunPengguna.Location = new System.Drawing.Point(505, 423);
+            this.akunPengguna.Name = "akunPengguna";
+            this.akunPengguna.Size = new System.Drawing.Size(40, 18);
+            this.akunPengguna.TabIndex = 9;
+            this.akunPengguna.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.akunPengguna);
+            this.Controls.Add(this.Instruction1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BantuanDetailInput);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.InputDetail);
+            this.Controls.Add(this.InputState);
+            this.Controls.Add(this.GenerateBTN);
+            this.Name = "Indihum Rich Presence Discord";
+            this.Text = "Indihum Rich Presence Discord";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenerateBTN;
+        private TextBox InputState;
+        private TextBox InputDetail;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+        private Label BantuanDetailInput;
+        private Label label3;
+        private Label Instruction1;
+        private Label akunPengguna;
+    }
+}
+
---
+++ Projek/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using DiscordRPC;
+
+namespace Goblox
+{
+    public partial class Form1 : Form
+    {
+        private delegate void SetTextCallback(string text);
+        private bool launched;
+        private bool debounce;
+        private bool processed;
+        public DiscordRpcClient client;
+        public DiscordRPC.Button addbotButton;
+        public DiscordRPC.Button fuckmeButton;
+
+        private void SetTextGenerateBTN(string text)
+        {
+            if (GenerateBTN.InvokeRequired)
+            {
+                var d = new SetTextCallback(SetTextGenerateBTN);
+                GenerateBTN.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                GenerateBTN.Text = text;
+            }
+        }
+        private void SetTextAkun(string text)
+        {
+            if (akunPengguna.InvokeRequired)
+            {
+                var d = new SetTextCallback(SetTextAkun);
+                akunPengguna.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                akunPengguna.Text = text;
+            }
+        }
+        public void wait(int milliseconds)
+        {
+            var timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+        public Form1()
+        {
+            debounce = true;
+            InitializeComponent();
+            client = new DiscordRpcClient("800007556810407946");
+            //client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+            //Subscribe to events
+            client.OnReady += (sendere, ee) =>
+            {
+                debounce = false;
+                SetTextAkun("Terhubung dengan, " + client.CurrentUser.ToString());
+                Console.WriteLine("Discord rich presence siap, {0}", ee.User.Username);
+            };
+            client.OnError += (err, something) =>
+            {
+                processed = false;
+                SetTextGenerateBTN("Gagal diperbarui..");
+                wait(850);
+                SetTextGenerateBTN("Perbarui");
+                debounce = false;
+            };
+            client.OnPresenceUpdate += (embed, something) =>
+            {
+                processed = false;
+                SetTextGenerateBTN("Berhasil diperbarui!");
+                wait(650);
+                SetTextGenerateBTN("Perbarui");
+                debounce = false;
+            };
+            client.Initialize();
+            addbotButton = new DiscordRPC.Button()
+            {
+                Url = "https://goblox.web.app",
+                Label = "Add Bot"
+            };
+            fuckmeButton = new DiscordRPC.Button()
+            {
+                Url = "https://goblox.web.app",
+                Label = "Add Bot"
+            };
+        }
+      
+        private void GenerateBTN_Click(object sender, EventArgs e)
+        {
+            if (debounce == true) return;
+            debounce = true;
+            client.SetPresence(new RichPresence()
+            {
+                Details = InputDetail.Text == "" ? "Menjadi tolol" : InputDetail.Text,
+                State = InputState.Text,
+                Timestamps = new Timestamps() { Start = System.DateTime.Now },
+                //Buttons = {[0] = addbotButton, [1] = fuckmeButton },
+                Assets = new Assets()
+                {
+                    LargeImageKey = "indihum2",
+                    LargeImageText = "Indihum helps me doin' funny things"
+                    //SmallImageKey = "indihum2"
+                }
+            });
+            processed = true;
+            if (launched != true)
+            {
+                launched = true;
+                var timer = new System.Timers.Timer(500);
+                timer.Elapsed += (sendere, args) => { client.Invoke(); };
+                timer.Start();
+            }
+
+            string titik = "";
+            do
+            {
+                if (processed == false) break;
+                if (titik.Length >= 4) titik = "";
+                titik += ".";
+                GenerateBTN.Text = "Memproses" + titik;
+                wait(175);
+                if (processed == false) break;
+            } while (processed == true);
+        }
+
+    }
+}
